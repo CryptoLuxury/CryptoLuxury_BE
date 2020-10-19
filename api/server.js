519ec67e2f107fb072907dacee9d777080515a7b
William <billy.ryan.rom@gmail.com>
2020-10-19 18:04:01 -0400

    Add contact routes and model * Add routes to server.js for router access through BE

--- api/server.js
+++ api/server.js
@@ -2,11 +2,33 @@ const express = require("express");
 const helmet = require('helmet')
 const cors = require('cors')
 
+const authUser = require('../users/authUsers-router.js')
+const authAdmin = require('../admins/authAdmins-router.js')
+const userRouter = require('../users/users-router.js')
+const adminRouter = require('../admins/admins-router.js')
+
+const contactRouter = require('../contact/contact-router.js')
+
+const cartsRouter = require('../store/carts-router.js')
+const cardsRouter = require('../store/cards-router.js')
+const watchesRouter = require('../store/watches-router.js')
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
+server.use('/api/auth', authAdmin);
+server.use('/api/accounts', adminRouter);
+server.use('/api/users', authUser);
+server.use('/api/accounts', userRouter);
+
+server.use('/api/form', contactRouter);
+
+server.use('/api/carts', cartsRouter);
+server.use('/api/cards', cardsRouter);
+server.use('/api/watches', watchesRouter);
 
 server.get("/", (req, res) => {
     res.status(200).json({ api: "up" });
---
+++ contact/contact-router.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const restricted = require('../admins/restricted-middleware')
+const Contact = require('./contact-model')
+
+const router = express.Router()
+
+router.get('/contact', restricted, (req, res) => {
+    
+    Contact.findAll()
+    .then(contact => {
+        res.status(200).json(contact)
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error Retrieving contacts' })
+    })
+})
+
+router.get('/contact/:id', restricted, (req, res) => {
+    const { id } = req.params;
+
+    Contact.findById(id)
+    .then(contact => {
+        if(contact.id >= 0){
+            return res.status(200).json(contact)
+        } else {
+            return res.status(404).json({ message: 'Error id invalid' })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error Retrieving contact' })
+    })
+})
+
+router.post('/contact', (req, res) => {
+    const data = req.body;
+
+    Contact.add(data)
+    .then(contact => {
+        res.status(201).json(contact)
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Failed to Create contact'})
+    })
+})
+
+router.put('/contact/:id', restricted, (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    Contact.findById(id)
+    .then(contact => {
+        if (contact){
+            contact.update(changes, id)
+            .then(updatedContact => {
+                res.status(200).json(updatedContact)
+            });
+        } else {
+            res.status(404).json({ message: 'Could not find specified contact'})
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error updating contact' })
+    })
+})
+
+router.delete('/contact', restricted, (req, res) => {
+
+    Contact.removeAll()
+    .then(() => {
+        res.status(200).json({ message: 'All contacts successfully deleted' })
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Unable to delete contacts' })
+    })
+})
+
+router.delete('/contact/:id', restricted, (req, res) => {
+    const { id } = req.params;
+
+    Contact.removeById(id)
+    .then(deleted => {
+        if(deleted) {
+            res.status(200).json({ message: 'contact removed' })
+        } else {
+            res.status(404).json({ message: 'Could not find specified contact' })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error deleting contact' })
+    })
+})
+
+module.exports = router
