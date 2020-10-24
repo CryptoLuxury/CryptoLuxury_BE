7944a1085ba07e2d2ffa9669277ee5d5542d6681
William <billy.ryan.rom@gmail.com>
2020-10-23 23:48:36 -0400

    Refactor stripe-router.js to correct input

--- stripe/stripe-router.js
+++ stripe/stripe-router.js
@@ -25,7 +25,7 @@ router.post('/create-checkout-session', async (req, res) => {
         price_data: {
           currency: 'usd',
           product_data: {
-            name: `${req.body.name}`,
+            name: `${req.body.title}`,
           },
           unit_amount: req.body.price,
         },
