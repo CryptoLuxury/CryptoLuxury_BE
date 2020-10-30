66686e948f8187fb3b52e5f8350c7e56300bd958
William <billy.ryan.rom@gmail.com>
2020-10-29 23:31:55 -0400

    Remove user_id foreign keys from tables

--- data/migrations/20201020005550_watchOrders.js
+++ data/migrations/20201020005550_watchOrders.js
@@ -1,15 +1,6 @@
 exports.up = function(knex) {
     return knex.schema.createTable('watchOrders', watchOrders => {
       watchOrders.increments();
-      
-      watchOrders
-        .integer('user_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('users')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
 
       watchOrders
         .integer('watch_id')
--- data/migrations/20201020005600_cardOrders.js
+++ data/migrations/20201020005600_cardOrders.js
@@ -1,15 +1,6 @@
 exports.up = function(knex) {
     return knex.schema.createTable('cardOrders', cardOrders => {
       cardOrders.increments();
-      
-      cardOrders
-        .integer('user_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('users')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
 
       cardOrders
         .integer('card_id')
