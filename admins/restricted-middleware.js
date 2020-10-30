e45f70bd899765ad5c1c0185979a057c481b6bcf
William <billy.ryan.rom@gmail.com>
2020-10-29 23:15:10 -0400

    Refactor middleware to simpler design

--- admins/restricted-middleware.js
+++ admins/restricted-middleware.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken"); // <<< install this npm package
 const { jwtSecret } = require("./secrets.js")
 
 module.exports = (req, res, next) => {
-  const { authorization, admin } = req.headers;
+  const { authorization } = req.headers;
   
-  if (authorization && admin === process.env.ADMIN_KEY) {
+  if (authorization) {
     jwt.verify(authorization, jwtSecret, (err, decodedToken) => {
       if(err){
         res.status(401).json({ message: 'invalid Credentials' })
